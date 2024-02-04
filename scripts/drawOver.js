daa3073a6c937ace21c94af5114d9200bf3b7d67
Salamandeenie <calisheason@gmail.com>
2024-02-04 13:50:31 -0500

    Fireplace 1

--- scripts/drawOver.js
+++ scripts/drawOver.js
@@ -8,7 +8,7 @@ document.addEventListener('keydown', function(event) {
   if (event.key >= '0' && event.key <= '9') {
     if (lastKey === event.key) {
         lastKey = undefined;
-        setImage(event.key + '.png'); // Sets the source of the holder div
+        setImage(event.key + '.gif'); // Sets the source of the holder div
     } else {
       lastKey = event.key;
     }
@@ -29,6 +29,15 @@ function setImage(path) {
   // Set holder's image source to the specified path
   holder.style.display = 'inline';  // or 'inline' or any other appropriate value
   holder.src = 'images/' + path;
+
+  // Handle image load errors
+  holder.onerror = function() {
+    console.error('Error loading image: ' + path);
+    // You can hide the holder or set a default image here
+    holder.style.display = 'none';
+    // Or you can set a default image like this
+    // holder.src = 'images/defaultImage.jpg';
+  };
 }
 
 function killPicture() {
